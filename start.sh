af8d48c9545f6e5036ec409f4353bb710ab5a67b
hoflyon <116480350+hoflyon@users.noreply.github.com>
2023-06-03 16:07:51 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PORT=80
-UUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
+a3bb939c-75b9-46a4-b5b5-e6a75a868d9a
 WebPage=https://bing.com
 CaddyConfig=https://raw.githubusercontent.com/bsefwe/Rail/main/etc/Caddyfile
 XRayConfig=https://raw.githubusercontent.com/bsefwe/Rail/main/etc/xray.json
